ff75bf92673a44a8c5ee43ffdf03e0b1609088c5
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-02-27 23:31:54 +0600

    keyboard enter button

--- js/app.js
+++ js/app.js
@@ -42,7 +42,8 @@ const displayPhones= (phones, dataLimit)=> {
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
+          <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+          </div>
       </div>
         `;
         phonesContainer.appendChild(phoneDiv);
@@ -59,13 +60,22 @@ const processSearch= (dataLimit) =>{
   loadPhone(searchText,dataLimit);
 }
 
-//handle search button click
+//handle search button clic
 document.getElementById('btn-search').addEventListener('click', function(){
     //start loader
   processSearch(10);
-
-
 })
+
+// search input enter key handler
+ document.getElementById('search-field').addEventListener('keypress', function(e){
+      //console.log(e.key);
+
+  if(e.key==='Enter'){
+    processSearch(10);
+     }
+ });
+
+
 const toggleSpinner= isLoading =>{
     const loaderSection= document.getElementById('loader')
     if(isLoading){
@@ -80,6 +90,14 @@ const toggleSpinner= isLoading =>{
 document.getElementById('btn-show-all').addEventListener('click', function(){
   processSearch();
 })
+
+const loadPhoneDetails=async id => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res= await fetch(url);
+  const data= await res.json();
+  console.log(data.data);
+
+}
 
 
 //loadPhone();
